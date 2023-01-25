2049b44b0dedb13f07b82f11a2bcd863273e3566
Adamdoescode <13943324+adamdoescode@users.noreply.github.com>
2023-01-25 10:00:48 +0800

    write CV_filler.py

---
+++ CV_filler.py
@@ -0,0 +1,62 @@
+# **************************************************************************** #
+#                                                                              #
+#                                                         :::      ::::::::    #
+#    CV_filler.py                                       :+:      :+:    :+:    #
+#                                                     +:+ +:+         +:+      #
+#    By: adam <adam@student.42.fr>                  +#+  +:+       +#+         #
+#                                                 +#+#+#+#+#+   +#+            #
+#    Created: 2023/01/25 09:31:07 by adam              #+#    #+#              #
+#    Updated: 2023/01/25 10:00:25 by adam             ###   ########.fr        #
+#                                                                              #
+# **************************************************************************** #
+
+'''
+A simple script to fill the CV with the data from the json file
+'''
+
+import json
+import sys
+import os
+
+#we can hardcode the path to the json file, assumes has a specific name and is in the same directory
+class CV_filler:
+    def __init__(self):
+        self.jsonData = "CV.json"
+        self.cvTemplate = "resume_text_blanks.md"
+    
+    def getJsonData(self):
+        with open(self.jsonData, "r") as jsonFile:
+            data = json.load(jsonFile)
+        return data['details']
+
+    def getCVTemplate(self):
+        with open(self.cvTemplate, "r") as cvFile:
+            data = cvFile.read()
+        return data
+    
+    def getDate(self):
+        '''date is easier to get from python'''
+        import datetime
+        return datetime.datetime.now().strftime("%Y-%m-%d")
+    
+    def fillCV(self):
+        data = self.getJsonData()
+        cv = self.getCVTemplate()
+        for key in data.keys():
+            cv = cv.replace(key, data[key])
+        #do the date separately
+        cv = cv.replace("{date}", self.getDate())
+        return cv
+
+    def writeCV(self, cv):
+        with open("resume_filled.md", "w") as cvFile:
+            cvFile.write(cv)
+    
+    def main(self):
+        cv = self.fillCV()
+        self.writeCV(cv)
+
+
+
+if __name__ == "__main__":
+    CV_filler().main()
--- resume_filled.md
+++ resume_filled.md
@@ -1,12 +1,12 @@
-{company address}
+default
 
-{date}
+{2023-01-25}
 
-To the team at {company name},
+To the team at default,
 
-I wish to apply for your advertised role of a {position} in {location}. I can provide your team with my years of experience in research. I am experienced in data analysis in a research context and am an enthusiastic student of biostatistics. I can provide {company name} with my experience in processing biological data. I am capable of processing datasets in Excel, R, python, and am very comfortable using the unix command line and various CLI tools such as grep/ripgrep, awk, and vim.
+I wish to apply for your advertised role of a default in default. I can provide your team with my years of experience in research. I am experienced in data analysis in a research context and am an enthusiastic student of biostatistics. I can provide default with my experience in processing biological data. I am capable of processing datasets in Excel, R, python, and am very comfortable using the unix command line and various CLI tools such as grep/ripgrep, awk, and vim.
 
-The skills I can bring to {company name} include:
+The skills I can bring to default include:
 
     • Experience in using R, python, the unix terminal, and building simple static websites
     • Experience accessing and processing data from online databases using APIs
