669f7c8b99f9f63554718c7b79dc039c2fd653ab
Adamdoescode <13943324+adamdoescode@users.noreply.github.com>
2023-02-14 16:16:22 +0800

    add CLI args and add templates for different roles

--- CV_filler.py
+++ CV_filler.py
@@ -6,7 +6,7 @@
 #    By: Adam Graham <13943324+adamdoescode@user    +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2023/01/25 09:31:07 by adam              #+#    #+#              #
-#    Updated: 2023/01/25 16:32:45 by Adam Graham      ###   ########.fr        #
+#    Updated: 2023/02/14 16:15:43 by Adam Graham      ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
@@ -16,53 +16,68 @@ A simple script to fill the CV with the data from the json file
 
 import json
 import sys
+import argparse
 import os
 
-#we can hardcode the path to the json file, assumes has a specific name and is in the same directory
+
+def parseArgs():
+    parser = argparse.ArgumentParser(
+        description='Fill CV with data from json file')
+    parser.add_argument(
+        '-i', '--input', help='path to json file', dest='jsonInput', default='input/CV.json')
+    parser.add_argument(
+        '-t', '--template', help='path to CV template', dest='templateInput', default='input/CV_template.md')
+    return parser.parse_args()
+
+# we can hardcode the path to the json file, assumes has a specific name
+# and is in the same directory
+
+
 class CV_filler:
-    def __init__(self):
-        self.jsonData = "CV.json"
-        self.cvTemplate = "CV_template.md"
-    
+    def __init__(self, jsonData="CV.json", cvTemplate="CV_template.md"):
+        self.jsonData = jsonData
+        self.cvTemplate = cvTemplate
+
     def getJsonData(self):
-        with open(f'input/{self.jsonData}', "r") as jsonFile:
+        with open(f'{self.jsonData}', "r") as jsonFile:
             data = json.load(jsonFile)
         return data['details']
 
     def getCVTemplate(self):
-        with open(f'input/{self.cvTemplate}', "r") as cvFile:
+        with open(f'{self.cvTemplate}', "r") as cvFile:
             data = cvFile.read()
         return data
-    
+
     def getDate(self):
         '''date is easier to get from python'''
         import datetime
         return datetime.datetime.now().strftime("%Y-%m-%d")
-    
+
     def fillCV(self):
         data = self.getJsonData()
         cv = self.getCVTemplate()
         for key in data.keys():
             cv = cv.replace(key, data[key])
-        #do the date separately
+        # do the date separately
         cv = cv.replace("{date}", self.getDate())
         return cv
 
     def writeCV(self, cv):
-        #get company name from json and replace spaces with _
+        # get company name from json and replace spaces with _
         company_name = self.getJsonData()['{company name}'].replace(' ', '_')
         self.outputFileName = f"output/{self.getDate()}_{company_name}_CV_filled"
         with open(f"{self.outputFileName}.md", "w") as cvFile:
             cvFile.write(cv)
-    
+
     def main(self):
         cv = self.fillCV()
         self.writeCV(cv)
 
 
-
 if __name__ == "__main__":
-    CVmaker = CV_filler()
-    CVmaker.main() #fill the CV, CV maker now has a file name to use
-    #use pandoc to convert md to docx
-    os.system(f"pandoc {CVmaker.outputFileName}.md -o {CVmaker.outputFileName}.docx")
+    args = parseArgs()
+    CVmaker = CV_filler(jsonData=args.jsonInput, cvTemplate=args.templateInput)
+    CVmaker.main()  # fill the CV, CV maker now has a file name to use
+    # use pandoc to convert md to docx
+    os.system(
+        f"pandoc {CVmaker.outputFileName}.md -o {CVmaker.outputFileName}.docx")
